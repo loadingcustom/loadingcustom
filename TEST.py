e176bc6325a878dba20051c157812478596cb0c8
loadingcustom <91027681+loadingcustom@users.noreply.github.com>
2024-06-23 23:20:31 +0700

    Update TEST.py

--- TEST.py
+++ TEST.py
@@ -3,29 +3,23 @@ import pyfiglet
 from colorama import Fore, init
 # warna
 red = Fore.LIGHTRED_EX
-cyan = Fore.LIGHTCYAN_EX
 blue = Fore.LIGHTBLUE_EX
 yellow = Fore.LIGHTYELLOW_EX
 white = Fore.WHITE
 bl = Fore.BLACK
 record = [{'Key': {'sourceCity': 'MLG', 'destinationCity': 'SBY', 'routePassed': ['SBY', 'MLG'], 'activityRewards': None}, 'Value': 30}, {'Key': {'sourceCity': 'SBY', 'destinationCity': 'SMG', 'routePassed': ['SMG', 'SBY'], 'activityRewards': None}, 'Value': 60}, {'Key': {'sourceCity': 'MLG', 'destinationCity': 'SMG', 'routePassed': ['SMG', 'MLG'], 'activityRewards': None}, 'Value': 12}, {'Key': {'sourceCity': 'SMG', 'destinationCity': 'CBN', 'routePassed': ['CBN', 'SMG'], 'activityRewards': None}, 'Value': 50}, {'Key': {'sourceCity': 'SBY', 'destinationCity': 'CBN', 'routePassed': ['CBN', 'SBY'], 'activityRewards': None}, 'Value': 10}, {'Key': {'sourceCity': 'MLG', 'destinationCity': 'CBN', 'routePassed': ['CBN', 'MLG'], 'activityRewards': None}, 'Value': 5}, {'Key': {'sourceCity': 'CBN', 'destinationCity': 'JKT', 'routePassed': ['JKT', 'CBN'], 'activityRewards': None}, 'Value': 45}, {'Key': {'sourceCity': 'SMG', 'destinationCity': 'JKT', 'routePassed': ['JKT', 'SMG'], 'activityRewards': None}, 'Value': 9}, {'Key': {'sourceCity': 'SBY', 'destinationCity': 'JKT', 'routePassed': ['JKT', 'SBY'], 'activityRewards': None}, 'Value': 5}, {'Key': {'sourceCity': 'MLG', 'destinationCity': 'JKT', 'routePassed': ['JKT', 'MLG'], 'activityRewards': None}, 'Value': 3}, {'Key': {'sourceCity': 'JKT', 'destinationCity': 'P_Merak', 'routePassed': ['P_Merak', 'JKT'], 'activityRewards': None}, 'Value': 45}]
+print(f"{yellow}••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••")
+
+print(f"{yellow}••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••")
+print(f"{blue}██████╗░██╗░░░██╗░██████╗░██████╗██╗██████╗░")
+print(f"{blue}██╔══██╗██║░░░██║██╔════╝██╔════╝██║██╔══██╗")
+print(f"{blue}██████╦╝██║░░░██║╚█████╗░╚█████╗░██║██║░░██║")
+print(f"{blue}██╔══██╗██║░░░██║░╚═══██╗░╚═══██╗██║██║░░██║")
+print(f"{blue}██████╦╝╚██████╔╝██████╔╝██████╔╝██║██████╔╝")
+print(f"{blue}╚═════╝░░╚═════╝░╚═════╝░╚═════╝░╚═╝╚═════╝░")
+print(f"{red}SERVER+")
 print(f"{yellow}--------------------------------------------------------------------------------")
-print(f"{red}██╗░██████╗██╗░░░██╗██████╗░██████╗░░█████╗░██████╗░████████╗░██████╗░██████╗")
-print(f"{red}██║██╔════╝██║░░░██║██╔══██╗██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝██╔════╝██╔════╝░")
-print(f"{red}██║╚█████╗░██║░░░██║██████╔╝██████╔╝██║░░██║██████╔╝░░░██║░░░╚█████╗░██║░░██╗░")
-print(f"{red}██║░╚═══██╗██║░░░██║██╔═══╝░██╔═══╝░██║░░██║██╔══██╗░░░██║░░░░╚═══██╗██║░░╚██╗")
-print(f"{red}██║██████╔╝╚██████╔╝██║░░░░░██║░░░░░╚█████╔╝██║░░██║░░░██║░░░██████╔╝╚██████╔╝")
-print(f"{red}╚═╝╚═════╝░░╚═════╝░╚═╝░░░░░╚═╝░░░░░░╚════╝░╚═╝░░╚═╝░░░╚═╝░░░╚═════╝░░╚═════╝░")
-print(f"{yellow}--------------------------------------------------------------------------------")
-print(f"{red}██████╗░██╗░░░██╗░██████╗░██████╗██╗██████╗░")
-print(f"{red}██╔══██╗██║░░░██║██╔════╝██╔════╝██║██╔══██╗")
-print(f"{red}██████╦╝██║░░░██║╚█████╗░╚█████╗░██║██║░░██║")
-print(f"{red}██╔══██╗██║░░░██║░╚═══██╗░╚═══██╗██║██║░░██║")
-print(f"{red}██████╦╝╚██████╔╝██████╔╝██████╔╝██║██████╔╝")
-print(f"{red}╚═════╝░░╚═════╝░╚═════╝░╚═════╝░╚═╝╚═════╝░")
-print(f"{cyan}SERVER1")
-print(f"{yellow}--------------------------------------------------------------------------------")
-auth = input(f'{red}[{white}#{red}] {red}Enter X-Authorization : {white} ')
+auth = input(f'{red}[{white}#{red}] {white}Enter : {white} ')
 
 headers = {'User-Agent': 'UnityEngine-Unity; Version: 2018.4.26f1','X-ReportErrorAsSuccess': 'true','X-PlayFabSDK': 'UnitySDK-2.20.170411','X-Authorization': '','Content-Type': 'application/json','Content-Length': '157','Host': '4ae9.playfabapi.com'}
 headers['X-Authorization'] = auth
